0df9b333351f8dcf42c881f0aaa99a92a7e5e602
Jairus Ralf Daclan <jairus977@gmail.com>
2020-04-03 20:23:30 +0800

    forgot active color

--- src/components/sections/Slider.js
+++ src/components/sections/Slider.js
@@ -4,12 +4,12 @@ import {sliderBG} from '../../constants'
 import arrowLeft from '../../img/arrow-left.png'
 import arrowRight from '../../img/arrow-right.png'
 
-function Dots({onChangeBg}) {
+function Dots({onChangeBg, bg}) {
   return (
     <div className="dots-container">
-      <button className="button-dot" label={sliderBG[0]} type="button" onClick={() => onChangeBg(0)} />
-      <button className="button-dot" label={sliderBG[1]} type="button" onClick={() => onChangeBg(1)} />
-      <button className="button-dot" label={sliderBG[2]} type="button" onClick={() => onChangeBg(2)} />
+      <button className={cn('button-dot', {active: bg === 0})} label={sliderBG[0]} type="button" onClick={() => onChangeBg(0)} />
+      <button className={cn('button-dot', {active: bg === 1})} label={sliderBG[1]} type="button" onClick={() => onChangeBg(1)} />
+      <button className={cn('button-dot', {active: bg === 2})} label={sliderBG[2]} type="button" onClick={() => onChangeBg(2)} />
     </div>
   )
 }
@@ -25,22 +25,24 @@ export default function Slider() {
   return (
     <section className="section section__slider">
       <div className={cn('slider', `bg--${sliderBG[bg]}`)}>
-        <button type="button" className="arrow-button" onClick={() => doChangeBg(null, -1)} disabled={bg === 0}>
-          <img src={arrowLeft} alt="left arrow" />
-        </button>
+        <div className="slider-content-container container">
+          <button type="button" className="arrow-button" onClick={() => doChangeBg(null, -1)} disabled={bg === 0}>
+            <img src={arrowLeft} alt="left arrow" />
+          </button>
 
-        <div className="slider-content">
-          <h1 className="title-slider">キャッチコピーが入ります。</h1>
-          <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
-          <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+          <div className="slider-content">
+            <h1 className="title-slider">キャッチコピーが入ります。</h1>
+            <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+            <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+          </div>
+
+          <button type="button" className="arrow-button" onClick={() => doChangeBg(null, 1)} disabled={bg === 2}>
+            <img src={arrowRight} alt="right arrow" />
+          </button>
         </div>
-
-        <button type="button" className="arrow-button" onClick={() => doChangeBg(null, 1)} disabled={bg === 2}>
-          <img src={arrowRight} alt="right arrow" />
-        </button>
       </div>
 
-      <Dots onChangeBg={doChangeBg} />
+      <Dots onChangeBg={doChangeBg} bg={bg} />
     </section>
   )
 }
