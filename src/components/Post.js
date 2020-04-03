3417ba6a61f1bd9ae2d76e2096bbee0c938eec23
Jairus Ralf Daclan <jairus977@gmail.com>
2020-04-03 21:29:14 +0800

    wip main content

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import ArticleImg from '../img/article.jpg'
 
 export default function Posts({data}) {
   return (
     <div className="post">
-      <img src="" alt="" />
+      <img src={ArticleImg} alt="" />
       <div className="post-content">
-        <p className="post-date">post date</p>
-        <h3>title</h3>
-        <p>content</p>
+        <p className="post-date">2016.01.01</p>
+        <h4 className="post-title">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</h4>
+        <p className="post-text">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト…</p>
       </div>
     </div>
   )
