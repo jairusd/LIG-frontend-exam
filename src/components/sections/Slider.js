3635280a24233261965783219848b8a3be4c372c
Jairus Ralf Daclan <jairus977@gmail.com>
2020-04-03 20:16:38 +0800

    done slider

--- src/components/sections/Slider.js
+++ src/components/sections/Slider.js
@@ -1,14 +1,46 @@
 import React, {useState} from 'react'
-import {sliderBG} from '../../constants'
 import cn from 'classnames'
+import {sliderBG} from '../../constants'
+import arrowLeft from '../../img/arrow-left.png'
+import arrowRight from '../../img/arrow-right.png'
+
+function Dots({onChangeBg}) {
+  return (
+    <div className="dots-container">
+      <button className="button-dot" label={sliderBG[0]} type="button" onClick={() => onChangeBg(0)} />
+      <button className="button-dot" label={sliderBG[1]} type="button" onClick={() => onChangeBg(1)} />
+      <button className="button-dot" label={sliderBG[2]} type="button" onClick={() => onChangeBg(2)} />
+    </div>
+  )
+}
 
 export default function Slider() {
-  const [bg, setBg] = useState(sliderBG[0])
+  const [bg, setBg] = useState(0)
+
+  const doChangeBg = (newBg, direction = 0) => {
+    if (!sliderBG[bg + direction]) return
+    setBg(newBg === null ? bg + direction : newBg)
+  }
+
   return (
     <section className="section section__slider">
-      <div className={cn('slider', `bg--${bg}`)}>
-        <h1>キャッチコピーが入ります。</h1>
+      <div className={cn('slider', `bg--${sliderBG[bg]}`)}>
+        <button type="button" className="arrow-button" onClick={() => doChangeBg(null, -1)} disabled={bg === 0}>
+          <img src={arrowLeft} alt="left arrow" />
+        </button>
+
+        <div className="slider-content">
+          <h1 className="title-slider">キャッチコピーが入ります。</h1>
+          <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+          <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+        </div>
+
+        <button type="button" className="arrow-button" onClick={() => doChangeBg(null, 1)} disabled={bg === 2}>
+          <img src={arrowRight} alt="right arrow" />
+        </button>
       </div>
+
+      <Dots onChangeBg={doChangeBg} />
     </section>
   )
 }
